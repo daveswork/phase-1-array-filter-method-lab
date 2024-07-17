436164f5bda817b7c59a51d1d8ffd5e744d1bca5
Dave J <29786887+daveswork@users.noreply.github.com>
2024-07-17 15:16:23 -0400

    All local tests pass.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+
+function findMatching(drivers, driverName){
+    return drivers.filter((driver) => driver.toUpperCase() === driverName.toUpperCase() );
+}
+
+function fuzzyMatch(drivers, partialName) {
+    return drivers.filter((driver) => driver.slice(0, partialName.length) === partialName);
+}
+
+
+function matchName(drivers, driverName) {
+    return drivers.filter((driver) => driver.name === driverName);
+}
